c3f84f4acbb439f0a9f982273ea942cd112b4a93
alienorar <rayhonakhayrullaeva@gmail.com>
2024-08-17 23:37:12 +0500

    first commit

--- main.js
+++ main.js
@@ -4,13 +4,14 @@ const student_model = document.getElementById("student-modal")
 const save_btn = document.getElementById("save")
 const cancel_btn = document.getElementById("cancel")
 const result = document.getElementById("result")
-let base_url = "http://localhost:3000/students?"
+let base_url = "http://localhost:3000/students"
 let form = {}
 let students_list = []
 let edit_student = -1
 let obj = {}
 let search = ""
 let page = 1
+let limit = 2
 document.addEventListener("DOMContentLoaded", function () {
     add_student.addEventListener("click", openModal)
     save_btn.addEventListener("click", saveStudent)
@@ -54,9 +55,12 @@ function handleChange(event) {
 
 }
 async function saveStudent() {
-    if (+edit_student > -1) {
+
+
+    if (typeof (edit_student) === "string") {
         try {
-            const response = await fetch(`${base_url}${edit_student}`, {
+            const response = await fetch(`http://localhost:3000/students/${edit_student}`, {
+
                 method: "PATCH",
                 headers: {
                     "Content-Type": "application/json"
@@ -65,6 +69,8 @@ async function saveStudent() {
 
 
             })
+            console.log(form);
+
 
         } catch (error) {
 
@@ -72,7 +78,7 @@ async function saveStudent() {
     }
     else {
         try {
-            const response = await fetch(`${base_url}`, {
+            const response = await fetch(`http://localhost:3000/students`, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
@@ -80,7 +86,6 @@ async function saveStudent() {
                 body: JSON.stringify(form)
 
             })
-            // console.log(response);
         } catch (error) {
             console.log(error);
 
@@ -91,8 +96,7 @@ async function saveStudent() {
 
 async function getStudent() {
     try {
-        const response = await fetch(`${base_url}_page=${page}&_limit=2`)
-        // console.log(response);
+        const response = await fetch(`http://localhost:3000/students`)
         students_list = await response.json()
         displayStudents()
     } catch (error) {
@@ -102,10 +106,10 @@ async function getStudent() {
     displayStudents()
 
 }
-function searchStudents(event) {
-    search = event.target.value
-    displayStudents()
-}
+// function searchStudents(event) {
+//     search = event.target.value
+//     displayStudents()
+// }
 
 function displayStudents() {
     result.innerHTML = ""
@@ -133,7 +137,7 @@ function displayStudents() {
 async function deleteStudent(id) {
 
     try {
-        await fetch(`${base_url}/${id}`, {
+        await fetch(`http://localhost:3000/students/${id}`, {
             method: "DELETE"
         })
         console.log(id);
@@ -147,6 +151,9 @@ async function deleteStudent(id) {
 
 async function editStudent(id) {
     edit_student = id
+    console.log(edit_student);
+    console.log(id);
+
     form = students_list.find((item) => item.id === id)
     document.querySelector("input[name='first_name']").value = form.first_name;
     document.querySelector("input[name='last_name']").value = form.last_name;
@@ -154,7 +161,8 @@ async function editStudent(id) {
     document.querySelector("input[name='phone_number']").value =
         form.phone_number;
     document.querySelector("input[name='date']").value = form.date;
-    document.querySelector("input[name='id']").value = form.id;
+
+
 
     console.log(form);
 
@@ -174,4 +182,3 @@ function openModal() {
 function closeModal() {
     toggleModal("none")
 }
-
